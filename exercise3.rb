e93515903743a97646f8ec243cf53f6d35a9f5d6
Thomas Spencer <t.spencer1990@hotmail.com>
2016-08-09 13:51:50 -0400

    Submission of exercise three

---
+++ exercise3.rb
@@ -0,0 +1,9 @@
+puts "Hello, whats your name?"
+
+name = gets.chomp
+
+puts "Hi, #{name}! How old are you?"
+
+age = gets.chomp
+
+puts "Wow #{name}, I never would have guessed you were #{age.to_i}"
